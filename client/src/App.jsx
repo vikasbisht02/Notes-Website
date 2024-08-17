5e5a5ff7863b93ec643d48b3ce36ef15f2b3243d
Vikas Bisht <160773046+vikasbisht02@users.noreply.github.com>
2024-08-17 06:02:31 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path='dashboard' exact element={<Home/>}/>
-        <Route path='login' exact element={<Login/>}/>
+        <Route path='/' exact element={<Login/>}/>
         <Route path='signup' exact element={<Signup/>}/>
       </Routes>
     </Router>
