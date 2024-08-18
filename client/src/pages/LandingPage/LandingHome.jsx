dee2a354e7e9412f8d3a44fd3cca82681617cd94
Vikas Bisht <160773046+vikasbisht02@users.noreply.github.com>
2024-08-18 12:08:58 -0500

    Update LandingHome.jsx

--- client/src/pages/LandingPage/LandingHome.jsx
+++ client/src/pages/LandingPage/LandingHome.jsx
@@ -7,16 +7,20 @@ const LandingHome = () => {
         <h1 className="text-2xl md:text-3xl font-medium text-black-600 text-center mb-0">
           Notes App
         </h1>
-        <h1 className="text-xl md:text-xl  font-medium text-black-600 text-center mt-0 mb-0">
+        <h1 className="text-xl md:text-xl font-medium text-black-600 text-center mt-0 mb-0">
           Write Your Important Things Here
         </h1>
         <div className="flex justify-center flex-col md:flex-row md:space-x-4 space-y-0 md:space-y-0 mt-0">
-          <button className="w-full  md:w-auto px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mb-3" >
-            <Link to="/login">Login</Link>
-          </button>
-          <button className="w-full md:w-auto px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600">
-            <Link to="/signup">Signup</Link>
-          </button>
+          <Link to="/login" className="w-full">
+            <button className="w-full md:w-full px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mb-3">
+              Login
+            </button>
+          </Link>
+          <Link to="/signup" className="w-full">
+            <button className="w-full md:w-full px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+              Signup
+            </button>
+          </Link>
         </div>
       </div>
     </div>
